1b7944477eb95760c7588fc7a12d2d430edb8323
bob <robertbalonek@gmail.com>
2021-05-19 09:55:11 -0700

    Base URL

--- client/src/services/api.config.js
+++ client/src/services/api.config.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const baseUrl = "https://rails-auth-pratice-boilerplate.herokuapp.com/";
+// const baseUrl = "https://rails-auth-pratice-boilerplate.herokuapp.com/";
 
-// const baseUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "https://rails-auth-pratice-boilerplate.herokuapp.com/"
-//     : "http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://rails-auth-pratice-boilerplate.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
